b0802a32cbffab47e028c146cc7abc66c40f165f
Roberto Nicoletti <roberto.urias@gmail.com>
2019-09-29 23:19:02 -0300

    Iniciando em serviço com Angular

--- udemy-es6-typescript-angular/angular/projetos/app2/src/app/home/home.component.ts
+++ udemy-es6-typescript-angular/angular/projetos/app2/src/app/home/home.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { OfertasService } from "../ofertas.service";
+import { log } from "util";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  selector: "app-home",
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.scss"],
+  providers: [OfertasService]
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
+  constructor(private ofertaService: OfertasService) {}
 
   ngOnInit() {
+    console.log(this.ofertaService.getOfertas());
   }
-
 }
---
+++ udemy-es6-typescript-angular/angular/projetos/app2/src/app/ofertas.service.ts
@@ -0,0 +1,6 @@
+export class OfertasService {
+  public getOfertas(): Array<string> {
+    let ofertas = ["Oferta 1", "Oferta 2", "OFerta 3"];
+    return ofertas;
+  }
+}
