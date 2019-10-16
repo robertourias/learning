1cbc2284dc0673ba545012fa51035d587cf41d8a
Roberto Nicoletti <roberto.urias@gmail.com>
2019-10-15 22:01:29 -0300

    utilizando serviço carregando atraves de parametro da rota

--- udemy-es6-typescript-angular/angular/projetos/app2/src/app/oferta/oferta.component.ts
+++ udemy-es6-typescript-angular/angular/projetos/app2/src/app/oferta/oferta.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
+import { OfertasService } from "../ofertas.service";
+import { Oferta } from "../shared/oferta.model";
 
 @Component({
-  selector: 'app-oferta',
-  templateUrl: './oferta.component.html',
-  styleUrls: ['./oferta.component.scss']
+  selector: "app-oferta",
+  templateUrl: "./oferta.component.html",
+  styleUrls: ["./oferta.component.scss"],
+  providers: [OfertasService]
 })
 export class OfertaComponent implements OnInit {
+  public oferta: Oferta;
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private ofertasService: OfertasService
+  ) {}
 
   ngOnInit() {
+    this.ofertasService
+      .getOfertasPorId(this.route.snapshot.params["id"])
+      .then((oferta: Oferta) => {
+        this.oferta = oferta;
+        console.log(this.oferta);
+      });
+    // this.route.params.subscribe((parametro: any) => {
+    //   console.log(parametro.id);
+    // });
   }
-
 }
--- udemy-es6-typescript-angular/angular/projetos/app2/src/app/ofertas.service.ts
+++ udemy-es6-typescript-angular/angular/projetos/app2/src/app/ofertas.service.ts
@@ -21,4 +21,11 @@ export class OfertasService {
       .toPromise()
       .then((resposta: any) => resposta.json());
   }
+
+  public getOfertasPorId(id: number): Promise<Oferta> {
+    return this.http
+      .get(`http://localhost:3000/ofertas/?id=${id}`)
+      .toPromise()
+      .then((resposta: any) => resposta.json()[0]);
+  }
 }
