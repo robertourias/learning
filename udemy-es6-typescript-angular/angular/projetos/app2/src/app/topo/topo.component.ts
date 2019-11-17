0aeae249dc1cf465c940755ea1f2efd9c66da030
Roberto NIcoletti <robertourias@brq.com>
2019-11-17 20:40:08 -0300

    Finalizando as aulas de Observable do Angular

--- udemy-es6-typescript-angular/angular/projetos/app2/src/app/topo/topo.component.ts
+++ udemy-es6-typescript-angular/angular/projetos/app2/src/app/topo/topo.component.ts
@@ -1,31 +1,59 @@
-import { Component, OnInit } from "@angular/core";
-import { OfertasService } from "src/app/ofertas.service";
-import { Oferta } from "src/app/shared/oferta.model";
-import { Observable } from "rxjs";
+import { Component, OnInit } from '@angular/core';
+import { OfertasService } from 'src/app/ofertas.service';
+import { Oferta } from 'src/app/shared/oferta.model';
+import { Observable, Subject, of } from 'rxjs';
+import { switchMap, debounceTime, distinctUntilChanged, catchError } from 'rxjs/operators';
 
 @Component({
-  selector: "app-topo",
-  templateUrl: "./topo.component.html",
-  styleUrls: ["./topo.component.scss"],
+  selector: 'app-topo',
+  templateUrl: './topo.component.html',
+  styleUrls: ['./topo.component.scss'],
   providers: [OfertasService]
 })
 export class TopoComponent implements OnInit {
   public ofertas: Observable<Oferta[]>;
+  public ofertas2: Oferta[];
+  private subjectPesquisa: Subject<string> = new Subject<string>();
   constructor(private ofertasService: OfertasService) {}
 
-  ngOnInit() {}
+  // public pesquisa(termoDaBusca: string): void {
+  //   this.ofertas = this.ofertasService.pesquisaOfertas(termoDaBusca);
+
+  //   this.ofertas.subscribe(
+  //     (ofertas: Oferta[]) => {
+  //       console.log(ofertas);
+  //     },
+  //     (erro: any) => {
+  //       console.log("Erro status: ", erro.status);
+  //     },
+  //     () => console.log("Fluxo de eventos completo")
+  //   );
+  // }
+
+  ngOnInit() {
+    this.ofertas = this.subjectPesquisa.pipe(
+      debounceTime(1000), // Executa ação switchMap depois de 1 segundo
+      distinctUntilChanged(),
+      switchMap((termo: string) => {
+        console.log('Requisição HTTP');
+        if (termo.trim() === '') {
+          return of<Oferta[]>([]);
+        }
+        return this.ofertasService.pesquisaOfertas(termo);
+      }),
+      catchError((err: any) => {
+        console.log(err);
+        return of<Oferta[]>([]);
+      })
+    );
+
+    this.ofertas.subscribe((ofertas: Oferta[]) => {
+      this.ofertas2 = ofertas;
+    });
+  }
 
   public pesquisa(termoDaBusca: string): void {
-    this.ofertas = this.ofertasService.pesquisaOfertas(termoDaBusca);
-
-    this.ofertas.subscribe(
-      (ofertas: Oferta[]) => {
-        console.log(ofertas);
-      },
-      (erro: any) => {
-        console.log("Erro status: ", erro.status);
-      },
-      () => console.log("Fluxo de eventos completo")
-    );
+    console.log('Keyup do caracter:', termoDaBusca);
+    this.subjectPesquisa.next(termoDaBusca);
   }
 }
