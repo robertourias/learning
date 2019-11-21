35208335cc3603c03d980bdfce9f63b3626026df
Roberto NIcoletti <robertourias@brq.com>
2019-11-21 14:27:29 -0300

    aula pipes

--- udemy-es6-typescript-angular/angular/projetos/app2/src/app/topo/topo.component.ts
+++ udemy-es6-typescript-angular/angular/projetos/app2/src/app/topo/topo.component.ts
@@ -15,33 +15,17 @@ export class TopoComponent implements OnInit {
   private subjectPesquisa: Subject<string> = new Subject<string>();
   constructor(private ofertasService: OfertasService) {}
 
-  // public pesquisa(termoDaBusca: string): void {
-  //   this.ofertas = this.ofertasService.pesquisaOfertas(termoDaBusca);
-
-  //   this.ofertas.subscribe(
-  //     (ofertas: Oferta[]) => {
-  //       console.log(ofertas);
-  //     },
-  //     (erro: any) => {
-  //       console.log("Erro status: ", erro.status);
-  //     },
-  //     () => console.log("Fluxo de eventos completo")
-  //   );
-  // }
-
   ngOnInit() {
     this.ofertas = this.subjectPesquisa.pipe(
       debounceTime(1000), // Executa ação switchMap depois de 1 segundo
       distinctUntilChanged(), // Para fazer pesquisas distintas
       switchMap((termo: string) => {
-        console.log('Requisição HTTP');
         if (termo.trim() === '') {
           return of<Oferta[]>([]);
         }
         return this.ofertasService.pesquisaOfertas(termo);
       }),
       catchError((err: any) => {
-        console.log(err);
         return of<Oferta[]>([]);
       })
     );
@@ -52,7 +36,6 @@ export class TopoComponent implements OnInit {
   }
 
   public pesquisa(termoDaBusca: string): void {
-    console.log('Keyup do caracter:', termoDaBusca);
     this.subjectPesquisa.next(termoDaBusca);
   }
 }
